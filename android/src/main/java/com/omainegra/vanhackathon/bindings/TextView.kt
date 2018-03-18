8670edfedf2483d4a612fd9473ac4c2a3d5c1d18
Omar Alejandro Mainegra Sarduy <omainegra@gmail.com>
2018-03-18 16:15:31 -0300

    Fix Databinding Adapters

--- android/src/main/java/com/omainegra/vanhackathon/bindings/TextView.kt
+++ android/src/main/java/com/omainegra/vanhackathon/bindings/TextView.kt
@@ -1,9 +1,8 @@
 package com.omainegra.vanhackathon.bindings
 
 import android.databinding.BindingAdapter
-import android.support.design.widget.TextInputLayout
+import android.widget.Button
 import android.widget.TextView
-import arrow.core.Option
 import io.reactivex.Observable
 
 /**
@@ -13,10 +12,10 @@ import io.reactivex.Observable
 
 object TextViewBindingAdapters : RxObservableBindingAdapter() {
 
-    @JvmStatic @BindingAdapter("android:enable")
-    fun setEnable(textView: TextView, errors: Observable<Boolean>){
-        putDisposable(textView, errors.subscribe {
-            textView.isEnabled = it
+    @JvmStatic @BindingAdapter("android:enabled")
+    fun setEnable(button: TextView, observable: Observable<Boolean>){
+        putDisposable(button, observable.subscribe {
+            button.isEnabled = it
         })
     }
 }

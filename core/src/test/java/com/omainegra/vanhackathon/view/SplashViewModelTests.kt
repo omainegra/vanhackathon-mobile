75ca041d6404e97baa63562c4a17e9c2d6491067
Omar Alejandro Mainegra Sarduy <omainegra@gmail.com>
2018-03-18 13:09:22 -0300

    Fix SplashViewModel test

--- core/src/test/java/com/omainegra/vanhackathon/view/SplashViewModelTests.kt
+++ core/src/test/java/com/omainegra/vanhackathon/view/SplashViewModelTests.kt
@@ -1,6 +1,8 @@
 package com.omainegra.vanhackathon.view
 
+import com.omainegra.vanhackathon.services.InMemoryKeyValueStore
 import com.omainegra.vanhackathon.services.NoOpInitializer
+import com.omainegra.vanhackathon.services.PreferencesImpl
 import io.kotlintest.matchers.shouldBe
 import io.kotlintest.specs.StringSpec
 import io.reactivex.schedulers.TestScheduler
@@ -12,9 +14,27 @@ import java.util.concurrent.TimeUnit
 
 class SplashViewModelTests : StringSpec({
 
-    "navigation should send to Home after initialize the Application" {
+    "navigation should send to Register if there is no access token in preferences" {
         val scheduler = TestScheduler()
-        val viewModel = SplashViewModelImpl(scheduler, NoOpInitializer())
+        val map: HashMap<String, Any> = HashMap()
+        val preferences = PreferencesImpl(InMemoryKeyValueStore(map), scheduler)
+        val viewModel = SplashViewModelImpl(scheduler, NoOpInitializer(), preferences)
+
+        viewModel.start()
+
+        val navigation = viewModel.navigation().test()
+        scheduler.advanceTimeBy(1, TimeUnit.SECONDS)
+
+        navigation.values() shouldBe listOf(SplashNavigation.Register)
+    }
+
+    "navigation should send to Home if there is an access token in preferences" {
+        val scheduler = TestScheduler()
+        val map: HashMap<String, Any> = HashMap()
+        map["ACCESS_TOKEN_PARAM"] = "access_token"
+
+        val preferences = PreferencesImpl(InMemoryKeyValueStore(map), scheduler)
+        val viewModel = SplashViewModelImpl(scheduler, NoOpInitializer(), preferences)
 
         viewModel.start()
 

1f8e64ef2e6b8e4811c54dbe4055d4216d92de18
Omar Alejandro Mainegra Sarduy <omainegra@gmail.com>
2018-03-18 20:04:16 -0300

    Fix Random Image Url

--- core/src/main/java/com/omainegra/vanhackathon/services/Network.kt
+++ core/src/main/java/com/omainegra/vanhackathon/services/Network.kt
@@ -132,7 +132,7 @@ object JsonMapping {
         val id = it.json.get("id").asLong
         val name = it.json.get("name").asString
         val address = it.json.get("address").asString
-        val imageUrl = "https://picsum.photos/1280/720/?random&id=$id}"
+        val imageUrl = "https://picsum.photos/1280/720/?randomId=$id&random=true"
 
         Store(id, name, address, imageUrl)
     }
